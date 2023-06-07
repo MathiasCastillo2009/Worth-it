d6938199be0b3b6f826e8051716e6de19f496004
MathiasCastillo2009 <mcastillo@crackthecode.la>
2023-06-07 16:51:52 -0500

    Gigachad added + script

---
+++ assets/JS/script.js
@@ -0,0 +1,24 @@
+//Operaciones matematics
+/* let x=5
+let y=9
+let z=2
+let suma=z+y+3
+let rest=z-3-1
+let divid=z/3/x
+let mult=z*3*y
+console.log("suma:",suma)
+console.log("resta:",rest)
+console.log("divide:",divid)
+console.log("multiplica:",mult) */
+var mensaje="HolA mUndo";
+var tamaño=mensaje.length;
+var sustraer=mensaje.substr(0,5);
+var edad=10
+var convertstring=edad.toString();
+var mayuscula=mensaje.toUpperCase();
+var minuscula=mensaje.toLowerCase();
+console.log("mensaje en numeros:", tamaño);
+console.log("mensaje de sustraer:", sustraer);
+console.log("convertir a string(numeritos):", convertstring);
+console.log("mayuscula",mayuscula);
+console.log("minuscula",minuscula);
---
+++ assets/JS/scriptpacman.js
@@ -0,0 +1,6 @@
+var canvas=document.getElementById("pacman");
+var contexto=canvas.getContext("2d");
+contexto.fillStyle = "rgb(200,17,230)";
+contexto.fillRect(10,10,55,50);
+contexto.strokeStyle = "rgb(100,340,200)";
+contexto.strokeRect(10,10,55,50)
